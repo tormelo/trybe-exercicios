2485928d5a2dbc0b3f0149e099a792b1a384b2e3
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-19 17:24:57 -0300

    adiciona resolucao da parte 2 do exercicio da aula 5.1

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -4,3 +4,12 @@ function changeText() {
   paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis a mi vel congue. Nulla facilisis diam eu massa viverra condimentum. Sed commodo consequat tortor nec pharetra. Proin vulputate, purus at dapibus scelerisque, nulla dolor blandit ex, a tempor sem lorem in elit. Sed tempus mollis mauris nec convallis. Phasellus eget massa magna. Curabitur imperdiet lectus eu volutpat congue. Aliquam non auctor odio. Aenean semper consectetur dolor, id posuere massa tempor at. Donec id nulla ut eros dignissim congue. Aliquam efficitur euismod nulla eu rhoncus. Nunc tortor tortor, rutrum a accumsan nec, volutpat sit amet ex. Vestibulum tristique magna turpis, ut congue felis pellentesque eu.';
 }
 changeText();
+
+// Exercício 2
+function changeColor() {
+  const mainContent = document.getElementsByClassName('main-content');
+  for (const element of mainContent) {
+    element.style.backgroundColor = 'rgb(76,164,109)';
+  }
+}
+changeColor();
