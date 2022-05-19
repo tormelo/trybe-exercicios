42fcb1f29cfba8797572b68fc120776fee748a70
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-19 16:29:05 -0300

    adiciona resolucao da parte 1 do exercicio da aula 5.1

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,6 @@
+// Exercício 1
+function changeText() {
+  const paragraph = document.getElementsByTagName('p')[0];
+  paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis a mi vel congue. Nulla facilisis diam eu massa viverra condimentum. Sed commodo consequat tortor nec pharetra. Proin vulputate, purus at dapibus scelerisque, nulla dolor blandit ex, a tempor sem lorem in elit. Sed tempus mollis mauris nec convallis. Phasellus eget massa magna. Curabitur imperdiet lectus eu volutpat congue. Aliquam non auctor odio. Aenean semper consectetur dolor, id posuere massa tempor at. Donec id nulla ut eros dignissim congue. Aliquam efficitur euismod nulla eu rhoncus. Nunc tortor tortor, rutrum a accumsan nec, volutpat sit amet ex. Vestibulum tristique magna turpis, ut congue felis pellentesque eu.';
+}
+changeText();
