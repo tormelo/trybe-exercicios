7cfe340e5508680d56c19a6686d32efb2daa1755
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-19 18:20:13 -0300

    adiciona resolucao da parte 3 do exercicio da aula 5.1

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,15 +1,20 @@
 // Exercício 1
-function changeText() {
-  const paragraph = document.getElementsByTagName('p')[0];
-  paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis a mi vel congue. Nulla facilisis diam eu massa viverra condimentum. Sed commodo consequat tortor nec pharetra. Proin vulputate, purus at dapibus scelerisque, nulla dolor blandit ex, a tempor sem lorem in elit. Sed tempus mollis mauris nec convallis. Phasellus eget massa magna. Curabitur imperdiet lectus eu volutpat congue. Aliquam non auctor odio. Aenean semper consectetur dolor, id posuere massa tempor at. Donec id nulla ut eros dignissim congue. Aliquam efficitur euismod nulla eu rhoncus. Nunc tortor tortor, rutrum a accumsan nec, volutpat sit amet ex. Vestibulum tristique magna turpis, ut congue felis pellentesque eu.';
+function setText(element, text) {
+  element.innerText = text;
 }
-changeText();
+
+const paragraph = document.getElementsByTagName('p')[0];
+const newText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis a mi vel congue. Nulla facilisis diam eu massa viverra condimentum. Sed commodo consequat tortor nec pharetra. Proin vulputate, purus at dapibus scelerisque, nulla dolor blandit ex, a tempor sem lorem in elit. Sed tempus mollis mauris nec convallis. Phasellus eget massa magna. Curabitur imperdiet lectus eu volutpat congue. Aliquam non auctor odio. Aenean semper consectetur dolor, id posuere massa tempor at. Donec id nulla ut eros dignissim congue. Aliquam efficitur euismod nulla eu rhoncus. Nunc tortor tortor, rutrum a accumsan nec, volutpat sit amet ex. Vestibulum tristique magna turpis, ut congue felis pellentesque eu.';
+setText(paragraph, newText);
 
 // Exercício 2
-function changeColor() {
-  const mainContent = document.getElementsByClassName('main-content');
-  for (const element of mainContent) {
-    element.style.backgroundColor = 'rgb(76,164,109)';
-  }
+function setBackgroundColor(element, color) {
+  element.style.backgroundColor = color;
 }
-changeColor();
+
+const mainContent = document.getElementsByClassName('main-content')[0];
+setBackgroundColor(mainContent, 'rgb(76,164,109)');
+
+// Exercício 3
+const centerContent = document.getElementsByClassName('center-content')[0];
+setBackgroundColor(centerContent, 'white');
