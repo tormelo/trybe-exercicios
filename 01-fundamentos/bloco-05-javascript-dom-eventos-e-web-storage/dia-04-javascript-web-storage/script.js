7fd06c09335cfc6cc11723eebb47729a010b9c8b
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-27 14:46:37 -0300

    adiciona funcoes de mudar font family e line height

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -13,10 +13,46 @@ fontColorPicker.addEventListener('change', () => {
 
 // Add event to change font size
 const fontSizeSlider = document.querySelector('#font-size');
-fontSizeSlider.min = 10;
-fontSizeSlider.max = 50;
+fontSizeSlider.min = 12;
+fontSizeSlider.max = 48;
 fontSizeSlider.value = 16;
 fontSizeSlider.addEventListener('change', () => {
   const content = document.querySelector('#content');
   content.style.fontSize = `${fontSizeSlider.value}px`;
+});
+
+
+// Add event to change line height
+const lineHeightSlider = document.querySelector('#line-height');
+lineHeightSlider.min = 100;
+lineHeightSlider.max = 200;
+lineHeightSlider.value = 110;
+lineHeightSlider.addEventListener('change', () => {
+  const content = document.querySelector('#content');
+  content.style.lineHeight = `${lineHeightSlider.value}%`;
+});
+
+// Add font family options
+const sansSerifFonts = ['Arial', 'Helvetica'];
+const serifFonts = [`'Times New Roman'`, 'Times'];
+const fontFamilySelect = document.querySelector('#font-family');
+
+function createFontOption(fontName, fontType) {
+  const option = document.createElement('option');
+  option.innerText = fontName.replace(/\'/g, '');
+  option.value = `${fontName}, ${fontType}`;
+  fontFamilySelect.appendChild(option);
+}
+
+for (const font of sansSerifFonts) {
+  createFontOption(font, 'sans-serif');
+}
+for (const font of serifFonts) {
+  createFontOption(font, 'serif');
+}
+
+fontFamilySelect.firstChild.selected = true;
+
+fontFamilySelect.addEventListener('change', () => {
+  document.body.style.fontFamily = fontFamilySelect.value;
 });
