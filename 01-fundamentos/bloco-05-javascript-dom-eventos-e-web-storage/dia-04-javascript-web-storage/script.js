36143aae88dabd6ef87512e46c2c167adddd85f5
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-27 13:30:12 -0300

    adiciona funcao de mudar tamanho da fonte

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,12 +1,22 @@
-// Change background color of the page
-const bkgColorButton = document.querySelector('#bkg-color');
-bkgColorButton.value = '#FFFFFF';
-bkgColorButton.addEventListener('change', () => {
-  document.body.style.backgroundColor = bkgColorButton.value;
+// Add event to change background color of the page
+const bkgColorPicker = document.querySelector('#bkg-color');
+bkgColorPicker.value = '#FFFFFF';
+bkgColorPicker.addEventListener('change', () => {
+  document.body.style.backgroundColor = bkgColorPicker.value;
 });
 
-// Change font color
-const fontColorButton = document.querySelector('#font-color');
-fontColorButton.addEventListener('change', () => {
-  document.body.style.color = fontColorButton.value;
+// Add event to change font color
+const fontColorPicker = document.querySelector('#font-color');
+fontColorPicker.addEventListener('change', () => {
+  document.body.style.color = fontColorPicker.value;
+});
+
+// Add event to change font size
+const fontSizeSlider = document.querySelector('#font-size');
+fontSizeSlider.min = 10;
+fontSizeSlider.max = 50;
+fontSizeSlider.value = 16;
+fontSizeSlider.addEventListener('change', () => {
+  const content = document.querySelector('#content');
+  content.style.fontSize = `${fontSizeSlider.value}px`;
 });
