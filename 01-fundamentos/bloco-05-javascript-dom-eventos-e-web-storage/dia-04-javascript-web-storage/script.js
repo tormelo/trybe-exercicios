5562c397ee9d38a5d35d521d565e37d420912f7a
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-27 13:16:44 -0300

    adiciona funcao de mudar cor da fonte

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -4,3 +4,9 @@ bkgColorButton.value = '#FFFFFF';
 bkgColorButton.addEventListener('change', () => {
   document.body.style.backgroundColor = bkgColorButton.value;
 });
+
+// Change font color
+const fontColorButton = document.querySelector('#font-color');
+fontColorButton.addEventListener('change', () => {
+  document.body.style.color = fontColorButton.value;
+});
