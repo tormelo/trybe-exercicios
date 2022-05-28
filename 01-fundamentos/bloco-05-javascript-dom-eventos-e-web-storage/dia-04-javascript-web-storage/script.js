d1fc014a913bae61a7080bec05d106dd08e3497e
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-27 21:41:04 -0300

    adiciona funcionalidade de salvamento automatico para exercicio 5.4

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,65 +1,105 @@
+const defaultSettings = {
+  backgroundColor: '#ffffff',
+  fontColor: '#000000',
+  fontSize: '16',
+  lineHeight: '120',
+  fontFamily: 'Arial, sans-serif',
+  textAlign: 'center',
+};
+let settings = {};
+
+const content = document.querySelector('#content');
+const bgColorPicker = document.querySelector('#bkg-color');
+const fontColorPicker = document.querySelector('#font-color');
+const fontSizeSlider = document.querySelector('#font-size');
+const lineHeightSlider = document.querySelector('#line-height');
+const fontFamilySelect = document.querySelector('#font-family');
+const aligmentSelect = document.querySelector('#text-align');
+
+fontSizeSlider.min = 12;
+fontSizeSlider.max = 48;
+lineHeightSlider.min = 100;
+lineHeightSlider.max = 200;
+
+// localStorage functions
+function saveSetting(key, value) {
+  settings[key] = value;
+  localStorage.settings = JSON.stringify(settings);
+}
+
+function loadSettings() {
+  if (localStorage.settings !== undefined) {
+    settings = JSON.parse(localStorage.settings);
+    updateElement(bgColorPicker, settings.backgroundColor);
+    updateElement(fontColorPicker, settings.fontColor);
+    updateElement(fontSizeSlider, settings.fontSize);
+    updateElement(lineHeightSlider, settings.lineHeight);
+    updateElement(fontFamilySelect, settings.fontFamily);
+    updateElement(aligmentSelect, settings.textAlign);
+  }
+  else {
+    resetSettings();
+  }
+}
+
+function updateElement(element, value) {
+  element.value = value;
+  const event = new Event('input');
+  element.dispatchEvent(event);
+}
+
+function resetSettings() {
+  localStorage.settings = JSON.stringify(defaultSettings);
+  loadSettings();
+}
+
 // Add event to change background color of the page
-const bkgColorPicker = document.querySelector('#bkg-color');
-bkgColorPicker.value = '#FFFFFF';
-bkgColorPicker.addEventListener('change', () => {
-  document.body.style.backgroundColor = bkgColorPicker.value;
+bgColorPicker.addEventListener('input', () => {
+  saveSetting('backgroundColor', bgColorPicker.value);
+  document.body.style.backgroundColor = settings.backgroundColor;
 });
 
 // Add event to change font color
-const fontColorPicker = document.querySelector('#font-color');
-fontColorPicker.addEventListener('change', () => {
-  document.body.style.color = fontColorPicker.value;
+fontColorPicker.addEventListener('input', () => {
+  saveSetting('fontColor', fontColorPicker.value);
+  document.body.style.color = settings.fontColor;
 });
 
 // Add event to change font size
-const fontSizeSlider = document.querySelector('#font-size');
-fontSizeSlider.min = 12;
-fontSizeSlider.max = 48;
-fontSizeSlider.value = 16;
-fontSizeSlider.addEventListener('change', () => {
-  const content = document.querySelector('#content');
-  content.style.fontSize = `${fontSizeSlider.value}px`;
+fontSizeSlider.addEventListener('input', () => {
+  saveSetting('fontSize', fontSizeSlider.value);
+  content.style.fontSize = `${settings.fontSize}px`;
 });
 
 
 // Add event to change line height
-const lineHeightSlider = document.querySelector('#line-height');
-lineHeightSlider.min = 100;
-lineHeightSlider.max = 200;
-lineHeightSlider.value = 110;
-lineHeightSlider.addEventListener('change', () => {
-  const content = document.querySelector('#content');
-  content.style.lineHeight = `${lineHeightSlider.value}%`;
+lineHeightSlider.addEventListener('input', () => {
+  saveSetting('lineHeight', lineHeightSlider.value);
+  content.style.lineHeight = `${settings.lineHeight}%`;
 });
 
 // Add font family options
-const sansSerifFonts = ['Arial', 'Helvetica'];
-const serifFonts = [`'Times New Roman'`, 'Times'];
-const fontFamilySelect = document.querySelector('#font-family');
+const fonts = [
+  {name: 'Arial', type: 'sans-serif'}, 
+  {name:'Helvetica', type: 'sans-serif'}, 
+  {name: `'Times New Roman'`, type: 'serif'}, 
+  {name: 'Times', type: 'serif'},
+];
 
-function createFontOption(fontName, fontType) {
+for (const font of fonts) {
   const option = document.createElement('option');
-  option.innerText = fontName.replace(/\'/g, '');
-  option.value = `${fontName}, ${fontType}`;
+  option.innerText = font.name.replace(/\'/g, '');
+  option.value = `${font.name}, ${font.type}`;
   fontFamilySelect.appendChild(option);
 }
 
-for (const font of sansSerifFonts) {
-  createFontOption(font, 'sans-serif');
-}
-for (const font of serifFonts) {
-  createFontOption(font, 'serif');
-}
-
-fontFamilySelect.firstChild.selected = true;
-
-fontFamilySelect.addEventListener('change', () => {
-  document.body.style.fontFamily = fontFamilySelect.value;
+fontFamilySelect.addEventListener('input', () => {
+  saveSetting('fontFamily', fontFamilySelect.value);
+  document.body.style.fontFamily = settings.fontFamily;
 });
 
 // Add text alignment options
 const alignments = ['Center', 'Left', 'Right', 'Justify'];
-const aligmentSelect = document.querySelector('#text-align');
 
 for (const alignment of alignments) {
   const option = document.createElement('option');
@@ -68,9 +108,16 @@ for (const alignment of alignments) {
   aligmentSelect.appendChild(option);
 }
 
-aligmentSelect.firstChild.selected = true;
-
-aligmentSelect.addEventListener('change', () => {
-  const content = document.querySelector('#content');
-  content.style.textAlign = aligmentSelect.value;
+aligmentSelect.addEventListener('input', () => {
+  saveSetting('textAlign', aligmentSelect.value);
+  content.style.textAlign = settings.textAlign;
 });
+
+// Add reset button event
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', () => {
+  resetSettings();
+});
+
+// Load saved settings
+loadSettings();
