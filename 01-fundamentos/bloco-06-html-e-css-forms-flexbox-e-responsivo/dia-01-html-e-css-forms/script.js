049d80b2fa81d2ac2d501e89b2427a814ff89df6
tormelo <23294699+tormelo@users.noreply.github.com>
2022-05-31 19:36:07 -0300

    adiciona resolucao dos exercicios bonus da aula 6.1

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -1,4 +1,5 @@
-const submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener('click', (event) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
   event.preventDefault();
+  alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
 });
