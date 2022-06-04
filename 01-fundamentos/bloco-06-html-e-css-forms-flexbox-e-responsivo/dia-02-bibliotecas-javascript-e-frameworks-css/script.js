a5b8e0c2002a9be4648c1df5325421644249b860
tormelo <23294699+tormelo@users.noreply.github.com>
2022-06-04 00:54:36 -0300

    adiciona resolucao do exercicio bonus 2 da aula 6.2

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -11,4 +11,85 @@ let picker = new Pikaday({
   defaultDate: minDate,
   minDate: minDate,
   maxDate: maxDate,
+  toString(date, format) {
+    // you should do formatting based on the passed format,
+    // but we will just return 'D/M/YYYY' for simplicity
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    // dateString is the result of `toString` method
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  },
+});
+
+const validate = new window.JustValidate('#form');
+
+validate
+.addField('#input-name', [
+  {
+    rule: 'minLength',
+    value: 10,
+    errorMessage: 'O campo precisa ter pelo menos 10 caracteres'
+  },
+  {
+    rule: 'maxLength',
+    value: 40,
+    errorMessage: 'O campo tem um limite de 40 caracteres'
+  },
+  {
+    rule: 'required',
+    errorMessage: 'Campo obrigatório'
+  }
+])
+.addField('#input-email', [
+  {
+    rule: 'required',
+    errorMessage: 'Campo obrigatório'
+  },
+  {
+    rule: 'email',
+    errorMessage: 'E-mail inválido!'
+  },
+])
+.addField('#input-answer', [
+  {
+    rule: 'minLength',
+    value: 140,
+    errorMessage: 'O campo precisa ter pelo menos 140 caracteres'
+  },
+  {
+    rule: 'maxLength',
+    value: 500,
+    errorMessage: 'O campo tem um limite de 500 caracteres'
+  },
+  {
+    rule: 'required',
+    errorMessage: 'Campo obrigatório'
+  }
+])
+.addField('#input-date', [
+  {
+    rule: 'required',
+    errorMessage: 'Campo obrigatório'
+  },
+  {
+    validator: () => Object.prototype.toString.call(picker.getDate()) === "[object Date]",
+    errorMessage: 'Data inválida'
+  }
+])
+.addField('#agreement', [
+  {
+    rule: 'required',
+    errorMessage: 'Campo obrigatório'
+  }
+])
+.onSuccess(() => {
+  alert('Formulário enviado com sucesso!');
 });
