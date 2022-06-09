4c086d866c37cdc70de16b915929bc7879e39911
tormelo <23294699+tormelo@users.noreply.github.com>
2022-06-09 16:28:59 -0300

    parte 1 - adiciona resolucao do exercicio 3

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/script.js
@@ -2,10 +2,10 @@ function validateNumbers(value1, value2) {
   if (!value1 || !value2) {
     throw new Error(`Os dois campos precisam ser preenchidos para realizar a soma.`)
   }
-  
-    if (isNaN(value1) || isNaN(value2)) {
-      throw new Error(`Informe apenas números nos campos.`)
-    }
+
+  if (isNaN(value1) || isNaN(value2)) {
+    throw new Error(`Informe apenas números nos campos.`)
+  }
 }
 
 function sum() {
@@ -17,10 +17,11 @@ function sum() {
 
     const result = parseInt(value1) + parseInt(value2);
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
   } catch (error) {
     document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
   }
 }
 
