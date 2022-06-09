e9e49c656e68a0facbb52980d9e9237eb37dcf7e
tormelo <23294699+tormelo@users.noreply.github.com>
2022-06-09 18:35:07 -0300

    parte 2 - adiciona resolucao do exercicio 2

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/script.js
@@ -9,19 +9,20 @@ function validateNumbers(value1, value2) {
 }
 
 function sum() {
+  const input1 = document.getElementById('value1');
+  const input2 = document.getElementById('value2');
+  const result = document.getElementById('result');
   try {
-    let value1 = document.getElementById('value1').value;
-    let value2 = document.getElementById('value2').value;
-
+    const value1 = input1.value;
+    const value2 = input2.value;
     validateNumbers(value1, value2);
-
-    const result = parseInt(value1) + parseInt(value2);
-    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+    const sum = parseInt(value1) + parseInt(value2);
+    result.innerText = `Resultado: ${sum}`;
   } catch (error) {
-    document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+    result.innerText = `Erro: ${error.message}`;
   } finally {
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
+    input1.value = '';
+    input2.value = '';
   }
 }
 
--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-2/parte-2-delivery.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-2/parte-2-delivery.js
@@ -46,8 +46,14 @@ const customerInfo = (order) => {
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  const newCustomer = order.name = 'Luiz Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drink = order.order.drinks.coke.type;
+  const newPrice = order.payment.total = 50;
 
+  console.log(`Olá ${newCustomer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drink} é R$ ${newPrice},00.`);
 }
 
 orderModifier(order);
+
+console.log(order);
