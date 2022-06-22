f0e70064c61682bcb2b2a5f83fbb7540a337eced
tormelo <23294699+tormelo@users.noreply.github.com>
2022-06-22 16:28:16 -0300

    adiciona resolucao do exercicio 1

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/agora-na-pratica/exercicio-1-cadastro-funcionario.js
@@ -0,0 +1,19 @@
+const register = (name) => {
+  const nameArray = name.toLowerCase().match(/\p{Letter}+/gu);
+  const employee = {
+    nomeCompleto: name,
+    email: `${nameArray.join('_')}@trybe.com`,
+  }
+  return employee;
+};
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(register));
