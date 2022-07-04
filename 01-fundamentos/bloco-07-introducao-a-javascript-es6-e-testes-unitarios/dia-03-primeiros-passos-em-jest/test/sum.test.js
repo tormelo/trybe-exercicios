eeadafad23ab4f67c5a2b6a1cbeca7e9390ac740
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-04 14:40:21 -0300

    aula 7.3 - implementa testes da funcao myFizzBuzz

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/test/sum.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/test/sum.test.js
@@ -2,10 +2,10 @@ const sum = require("../src/sum");
 
 describe('1 - Função sum', () => {
   it('Para os argumentos 4 e 5 deve retornar 9', () => {
-    expect(sum(4, 5)).toEqual(9);
+    expect(sum(4, 5)).toBe(9);
   });
   it('Para ambos os argumentos como 0 deve retornar 0', () => {
-    expect(sum(0, 0)).toEqual(0);
+    expect(sum(0, 0)).toBe(0);
   });
   it('Para os argumentos 4 e "5" deve lançar um erro', () => {
     expect(() => sum(4, '5')).toThrow();
