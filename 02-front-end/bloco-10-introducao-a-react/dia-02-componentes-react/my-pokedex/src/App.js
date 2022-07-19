1d0715c2944e22c64df22c53375e9b94a70206cb
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-19 15:32:39 -0300

    10.2 - cria app my-pokedex com comando create-react-app e faz cleanup de arquivos desnecessarios

---
+++ 02-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <span>Inicializando app my-pokedex...</span>
+    );
+  }
+}
+
+export default App;
