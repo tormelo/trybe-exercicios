b3fb9b2878f09ea06dc59e4d54ae7015c436a00d
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-21 18:49:06 -0300

    11.1 - modifica componente Pokedex para mostrar somente um pokemon por vez

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.jsx
@@ -3,11 +3,33 @@ import Pokemon from "./Pokemon";
 import pokemons from "../data";
 
 class Pokedex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      index: 0,
+      maxIndex: pokemons.length - 1
+    }
+
+    this.moveIndex = this.moveIndex.bind(this);
+  }
+
+  moveIndex() {
+    this.setState(({ index: prevIndex, maxIndex }) => ({
+      index: prevIndex < maxIndex ? prevIndex + 1 : 0,
+    }));
+  }
+
   render() {
+    const { index } = this.state;
+    const pokemon = pokemons[index];
+
     return (
       <div className="pokedex">
-        {pokemons.map((pokemon) => <Pokemon key={pokemon.id} info={pokemon} />)}
+        <Pokemon key={pokemon.id} info={pokemon} />
+        <button onClick={this.moveIndex}>Próximo pokémon</button>
       </div>
+
     );
   }
 }
