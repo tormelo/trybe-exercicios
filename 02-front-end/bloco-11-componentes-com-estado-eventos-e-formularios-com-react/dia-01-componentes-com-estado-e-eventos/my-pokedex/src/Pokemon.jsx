dc110da2421c3b1a913c679e9096fbdbdd8dc8b0
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-21 17:51:34 -0300

    11.1 - refatora componente Pokemon desconstruindo as PropTypes utilizadas

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/Pokemon.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/Pokemon.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import PropTypes from 'prop-types'
+import { shape, string, number } from 'prop-types'
 
 class Pokemon extends Component {
   render() {
@@ -18,14 +18,14 @@ class Pokemon extends Component {
 };
 
 Pokemon.propTypes = {
-  info: PropTypes.shape({
-    name: PropTypes.string,
-    type: PropTypes.string,
-    averageWeight: PropTypes.shape({
-      value: PropTypes.number,
-      measurementUnit: PropTypes.string
+  info: shape({
+    name: string,
+    type: string,
+    averageWeight: shape({
+      value: number,
+      measurementUnit: string
     }),
-    image: PropTypes.string,
+    image: string,
   })
 }
 
