f7696d1ada9c67fa60b0f48fc8987b4a50ef7401
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-22 19:36:03 -0300

    11.1 - refatora componente Pokedex para renderizar botoes de filtro de forma dinamica

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.jsx
@@ -23,27 +23,27 @@ class Pokedex extends Component {
   }
 
   filterList(filter) {
-    this.setState(() => {
-      const filteredList = filter ? pokemons.filter(({ type }) => type === filter) : pokemons;
-      return ({
-        index: 0,
-        pkmnList: filteredList
-      })
+    const filteredList = filter === 'All' ? pokemons : pokemons.filter(({ type }) => (type === filter));
+    this.setState({
+      index: 0,
+      pkmnList: filteredList
     });
   }
 
   render() {
     const { index, pkmnList } = this.state;
     const pokemon = pkmnList[index];
+    const filters = pokemons.reduce((array, { type }) => (
+      array.includes(type) ? array : [...array, type]
+    ), ['All']);
+    const filterButtons = filters.map((filter) => (
+      <Button key={filter} handleClick={() => this.filterList(filter)}>{filter}</Button>
+    ));
 
     return (
       <div className="pokedex">
         <Pokemon key={pokemon.id} info={pokemon} />
-        <div>
-          <button onClick={() => this.filterList('')}>All</button>
-          <button onClick={() => this.filterList('Fire')}>Fire</button>
-          <button onClick={() => this.filterList('Psychic')}>Psychic</button>
-        </div>
+        <div>{filterButtons}</div>
         <Button handleClick={this.moveIndex}>Próximo pokémon</Button>
       </div>
 
