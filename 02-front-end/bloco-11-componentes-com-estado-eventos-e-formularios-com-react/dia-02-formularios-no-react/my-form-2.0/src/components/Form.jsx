9f2b2c5142851fdbadd9ae8030ce94ec121c2484
tormelo <23294699+tormelo@users.noreply.github.com>
2022-07-27 14:35:32 -0300

    11.2 - cria app react com create-react-app e limpa arquivos desnecessarios

---
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Form extends React.Component{
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <legend>Dados pessoais:</legend>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default Form;
